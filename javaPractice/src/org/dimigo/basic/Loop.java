a711111fb870b4e839ec3441763ec72e1d66dc71
Admin <Admin@User>
2015-03-27 16:45:03 +0900

    실습과제

---
+++ javaPractice/src/org/dimigo/basic/Loop.java
@@ -0,0 +1,22 @@
+package org.dimigo.basic;
+
+public class Loop {
+
+	public static void main(String[] args) {
+		
+		int i;
+		for(i=1; i<=10; i++){
+			for(int j=1; j<=10 ; j++){
+				if(i==j){
+					System.out.printf("*");
+				}else {
+					System.out.printf("%d",j);
+				}
+				System.out.println("");
+			}
+		}
+			
+
+	}
+
+}
